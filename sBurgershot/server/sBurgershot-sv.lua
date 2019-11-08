aec3ed4b1fc3c0281fab40b5ea02eca1a3469ebc
Slaughteror <48096907+Slaughteror@users.noreply.github.com>
2019-11-08 10:23:30 +0100

    Meilleur gestion

--- sBurgershot/server/sBurgershot-sv.lua
+++ sBurgershot/server/sBurgershot-sv.lua
@@ -1,29 +1,19 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 RegisterServerEvent('BuyBigmac')
-AddEventHandler('BuyBigmac', function()
+AddEventHandler('Buy', function(item)
     local _source = source
     local xPlayer  = ESX.GetPlayerFromId(source)
-    local price = Config.PriceBigmac
-    xPlayer.removeMoney(price)
-    xPlayer.addInventoryItem('bigmac', 1)
-end)
-RegisterServerEvent('BuyCola')
-AddEventHandler('BuyCola', function()
-    local _source = source
-    local xPlayer  = ESX.GetPlayerFromId(source)
-    local price = Config.PriceCola
-    xPlayer.removeMoney(price)
-    xPlayer.addInventoryItem('cola', 1)
-end)
-RegisterServerEvent('BuyMenu')
-AddEventHandler('BuyMenu', function()
-    local _source = source
-    local xPlayer  = ESX.GetPlayerFromId(source)
-    local price = Config.PriceMenu
-    xPlayer.removeMoney(price)
-    xPlayer.addInventoryItem('bigmac', 1)
-    xPlayer.addInventoryItem('cola', 1)
-    xPlayer.addInventoryItem('donut', 1)
+    If item ~= "Menu" then
+        local price = Config.Price[item]
+        xPlayer.removeMoney(price)
+        xPlayer.addInventoryItem(item, 1)
+    else
+        local price = Config.Price["Menu"]
+        xPlayer.removeMoney(price)
+        xPlayer.addInventoryItem('bigmac', 1)
+        xPlayer.addInventoryItem('cola', 1)
+        xPlayer.addInventoryItem('donut', 1)
+    end
 end)
 
