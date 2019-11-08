aac372ccb4999c85f023a53d754330c4347eb8f4
Slaughteror <48096907+Slaughteror@users.noreply.github.com>
2019-11-08 12:53:08 +0100

    Update __resource.lua

--- sBurgershot/__resource.lua
+++ sBurgershot/__resource.lua
@@ -1,4 +1,4 @@
-resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 name 'NativeUILua-Reloaded'
 description 'NativeUILua-Reloaded is UI library for FiveM designed specifically for making interface based on GTA:Online style. (Base source: https://github.com/FrazzIe/NativeUILua ) | (Base NativeUI : https://github.com/Guad/NativeUI) '
